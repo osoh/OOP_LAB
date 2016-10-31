aed4885287e1c27736e0f60745b55d518b536c4e
osoh <b27th@mail.ru>
2016-10-31 18:53:56 +0200

    Revert "Лаб2"
    This reverts commit 40403b0c428078aeea3ea6bf596ffd0954ad65a5.

--- Laba_1/src/Laba_1.java
+++ Laba_1/src/Laba_1.java
@@ -1,32 +1,33 @@
-import javafx.scene.layout.Border;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.event.ActionEvent*;
-import java.awt.event.ActionListener*;
-
-
-
-
-public class Lab2 {
+public class Laba_1 {
     public static void main(String[]args){
-        JFrame frame = new JFrame("second");
-        JButton btn1 = new JButton("connect");
-        JButton btn2 = new JButton("format");
-        JButton btn3 = new JButton("delete");
-        JButton btn4 = new JButton("record");
-        JPanel panel = new JPanel();
-        Usbcard usbcard = new Usbcard();
-
+        JFrame frame = new JFrame("first");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
         frame.setVisible(true);
-
-        frame.add(btn1, BorderLayout.NORTH);
-        frame.add(btn2, BorderLayout.EAST);
-        frame.add(btn3, BorderLayout.CENTER);
-        frame.add(btn4, BorderLayout.WEST);
-
+        JButton btn = new JButton("Result");
+        JTextField str1 = new JTextField();
+        JTextField str2 = new JTextField();
+        JTextField res = new JTextField();
+        GridLayout gbl = new GridLayout(4,0);
+        frame.setLayout(gbl);
+        frame.getContentPane().add(str1);
+        frame.getContentPane().add(str2);
+        frame.getContentPane().add(btn);
+        frame.getContentPane().add(res);
+        frame.setSize(400,400);
+        btn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String a;
+                String b;
+                a = str1.getText();
+                b = str2.getText();
+                for (int i = 0; i < b.length(); i++) {
+                    a = a.replaceAll(b.charAt(i) + "", "");
+                    res.setText(a);
+                }
+            }
+        });
     }
 }
